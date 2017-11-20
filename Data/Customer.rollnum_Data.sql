c6ed7a8294028acb13381d22e2c8472534187900
akashsgupta <akashsgupta96@gmail.com>
2017-11-20 19:29:26 +0530

    Update Customer.rollnum_Data.sql

--- Data/Customer.rollnum_Data.sql
+++ Data/Customer.rollnum_Data.sql
@@ -1,2 +1,3 @@
 INSERT INTO [Customer].[rollnum] ([roll]) VALUES (N'1         ')
 INSERT INTO [Customer].[rollnum] ([roll]) VALUES (N'2         ')
+INSERT INTO [Customer].[rollnum] ([roll]) VALUES (N'3         ')
