0155a2a47bae38007ec1950829614894d567e0a1
Akash <akashsgupta96@gmail.com>
2017-11-20 18:01:48 +0530

    name commit

--- Tables/dbo.name.sql
+++ Tables/dbo.name.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[name]
 (
-[Name] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Name] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[num] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
